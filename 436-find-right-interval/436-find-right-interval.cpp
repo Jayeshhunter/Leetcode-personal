5d09f988563c654fe3fe36df4f743bb87fc90212
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2022-03-02 23:57:06 +0530

    Time: 188 ms (16.82%), Space: 30.2 MB (22.06%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -11,11 +11,11 @@ public:
         int l=0;
         int idx=-1;
         int r=intervals.size()-1;
-        int res=INT_MAX;
+        
         while(l <= r){
           int mid = l+(r-l)/2;
           if(arr[mid][0] >= intervals[i][1]){
-            res = arr[mid][0];
+            
             idx = arr[mid][1];
             r = mid-1;  
           }else if(arr[mid][0] < intervals[i][1]){
