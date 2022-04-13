ad0d7834a7ad173c8a6863cf6d4958bfe68be5d6
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2022-04-13 16:41:51 +0530

    Time: 2 ms (38.70%), Space: 6.6 MB (18.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -11,10 +11,10 @@ public:
                 ans[i][c2]=++val;
             }
             if(c1 < c2 && r1 < r2){
-                for(int i=c2-1;i>c1;i--){
+                for(int i=c2-1;i>=c1;i--){
                     ans[r2][i] = ++val;
                 }
-                for(int i=r2;i>r1;i--){
+                for(int i=r2-1;i>=r1+1;i--){
                     ans[i][c1] = ++val;
                 }
             }
