098724da5273bd1014d79932bcaf78d78bbd25ee
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2022-06-23 17:08:00 +0530

    Time: 420 ms (33.77%), Space: 56.2 MB (58.52%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(),courses.end(),[](auto &a,auto &b){return a[1] < b[1];});
+        priority_queue<int>pq;
+        int total=0;
+        for(auto &it:courses){
+            int dur = it[0],end=it[1];
+            if(dur+total <= end){
+                total += dur;
+                pq.push(dur);
+            }else if(pq.size() && pq.top() > dur){
+                total += dur - pq.top();
+                pq.pop();
+                pq.push(dur);
+            }
+        }
+        return pq.size();
+    }
+};
